3113edd4c33b6c1478ac50d6f09f451e1bd565d5
Zane (She/They) <55933131+PythonCoderUnicorn@users.noreply.github.com>
2021-11-09 16:57:19 -0700

    pairwise t-test

--- Trek_functions.R
+++ Trek_functions.R
@@ -121,3 +121,10 @@ diameter_to_radius(142984)
 
 
 
+# pairwise t-test
+attach(airquality)
+Month <- factor(Month, labels = month.abb[5:9])
+pairwise.t.test(Ozone, Month)
+pairwise.t.test(Ozone, Month, p.adj = "bonf")
+pairwise.t.test(Ozone, Month, pool.sd = FALSE)
+detach()
