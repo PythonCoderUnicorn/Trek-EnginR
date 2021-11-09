f44a01a38e7fad2707aef419ff47249ce6577169
Zane (She/They) <55933131+PythonCoderUnicorn@users.noreply.github.com>
2021-11-09 07:34:28 -0700

    gravity function

--- Trek_functions.R
+++ Trek_functions.R
@@ -37,3 +37,50 @@ recalibration = function(x){
   }
 }
 recalibration(8)
+
+
+
+
+
+
+
+# Newton's gravity_law  
+# F_g = G*(m1 * m2)/R^2
+# F_g = gravity force between mass 1 and mass 2
+# G = universal gravitational constant (6.67e-11 N m^2/kg^2)
+# m1 = mass 1 (kg)
+# m2 = mass 2 (kg)
+# R^2 = distance from center of mass 1 to center of mass 2
+
+
+# calculate the force of gravity between the Sun and planet Uranus
+
+gravity_constant = 6.67e-11 #N m^2/kg^2
+
+# m1, m2
+Sun_mass_kg = 2e30 # kg
+Uranus_mass_kg = 8.7e25
+
+# R
+Uranus_distance_Sun_km = 2.87e9 # 2.74e9 to 3.01e9 km
+
+# convert km to m
+R_meters = Uranus_distance_Sun_km *1000
+
+
+gravity_force = function(m1,m2,R){
+  # F_g = G*(m1 * m2)/R^2
+  force = (gravity_constant) * ((m1 * m2)/ R^2)
+  glue::glue("{formatC(force,format='e', digits=2 )} N")
+}
+
+gravity_force(m1= Sun_mass_kg, m2= Uranus_mass_kg, R= R_meters)
+
+
+
+
+
+
+
+
+
